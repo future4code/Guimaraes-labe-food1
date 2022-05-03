3bd19659bf15c7bdce46ded3d5ce27648382e2a6
Renan Arimatea <renanmoraiz@gmail.com>
2022-05-03 18:57:55 -0300

    Criado o arquivo Base url da aplicação.

---
+++ src/routes/coordinator.js
@@ -0,0 +1,31 @@
+export const goToLogin = (history) => {
+    history.pushState("/rappi4/login")
+}
+
+export const goToSignUp = (history) => {
+    history.pushState("/rappi4/sign-up")
+}
+
+export const goToPerfil = (history) => {
+    history.pushState("/rappi4/perfil")
+}
+
+export const goToBusca = (history) => {
+    history.pushState("/rappi4/busca")
+}
+
+export const goToResultado = (history) => {
+    history.pushState("/rappi4/resultado")
+}
+
+export const goToCarrinho = (history) => {
+    history.pushState("/rappi4/carrinho")
+}
+
+export const goToEditarDados = (history) => {
+    history.pushState("/rappi4/editar-dados")
+}
+
+export const gotToCadastroEndereco = (history) => {
+    history.pushState("/rappi4/cadastro-endereco")
+}
