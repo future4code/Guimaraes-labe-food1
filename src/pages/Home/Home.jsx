afe3b59bf45f947e352f37eeee6dfde7d89f0cdd
jucelia xavier da silva <juceliaxavier29@gmail.com>
2022-05-03 20:58:32 -0300

    home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import { goToLoginPage } from "../../routes/coordinator";
 
 const Home = () => {
+    const navigate = useNavigate();
     return (
         <div>
-            <p>Home</p>
+            <div>
+
+               <div onClick={() => goToLoginPage(navigate)}>
+                <h1>Rappi4A</h1>
+               </div>
+
+          </div>
+
         </div>
     )
 }
