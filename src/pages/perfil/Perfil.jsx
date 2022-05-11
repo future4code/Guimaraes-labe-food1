54fa61ebe21c36c4056e2ccdbdee7b8def12c61f
BuildTools <caiorigotto@gmail.com>
2022-05-11 20:31:16 -0300

    Userequestdata e página de perfil funcionando (testado com versão diferente da página de login)

--- src/pages/perfil/Perfil.jsx
+++ src/pages/perfil/Perfil.jsx
@@ -8,37 +8,68 @@ import ProfileImg from '../../img/profile-edit.svg'
 
 const Perfil = () => {
     const profileData = useRequestData(`${BASE_URL}/profile`)
+    const orderHistory = useRequestData(`${BASE_URL}/orders/history`)
 
-    const onClickEditName = () =>{
+    const orders = orderHistory[0].orders
+
+    console.log(profileData)
+    console.log(orders)
+
+    const onClickEditName = () => {
         alert('Editando nome!')
     }
-    const onClickEditEmail = () =>{
+    const onClickEditEmail = () => {
         alert('Editando e-mail!')
     }
-    
+
+    const renderOrders = () => {
+        switch (orderHistory[2]) {
+            case false:
+                if (orders.length === 0) {
+                    return <p>Faça um pedido!</p>
+                } else
+                    if (orders.length > 0) {
+                        return <p>Pedidos:</p>
+                    }
+            case true:
+                return <p>Carregando...</p>
+        }
+    }
+
+    const loadingScreen = () => {
+        switch (profileData[2]) {
+            case false:
+                return <ContainerProfilePage>
+                    <Bar>
+                        <Title>
+                            <TitleTextStyle>Meu Perfil</TitleTextStyle>
+                        </Title>
+                    </Bar>
+                    <EditContainer>
+                        <ProfileText>{profileData[0].user.name}</ProfileText>
+                        <img src={ProfileImg} onClick={onClickEditName} />
+                    </EditContainer>
+                    <ProfileText>{profileData[0].user.email}</ProfileText>
+                    <ProfileText>{profileData[0].user.cpf}</ProfileText>
+                    <ProfileAdressContainer>
+                        <EditContainer>
+                            <ProfileAdressDesc>Endereço cadastrado</ProfileAdressDesc>
+                            <img src={ProfileImg} onClick={onClickEditEmail} />
+                        </EditContainer>
+                        <ProfileAdress>{profileData[0].user.address}</ProfileAdress>
+                    </ProfileAdressContainer>
+                    <DeliveryHistText>Histórico de pedidos</DeliveryHistText>
+                    <Line></Line>
+                    <div>{renderOrders()}</div>
+                </ContainerProfilePage>
+            case true:
+            default:
+                return <p>Carregando</p>
+        }
+    }
+
     return (
-        <ContainerProfilePage>
-            <Bar>
-                <Title>
-                <TitleTextStyle>Meu Perfil</TitleTextStyle>
-                </Title>
-            </Bar>
-            <EditContainer>
-            <ProfileText>Placeholder Name</ProfileText>
-            <img src={ProfileImg} onClick={onClickEditName}/>
-            </EditContainer>
-            <ProfileText>placeholder@gmail.com</ProfileText>
-            <ProfileText>333.333.333-33</ProfileText>
-            <ProfileAdressContainer>
-                <EditContainer>
-                <ProfileAdressDesc>Endereço cadastrado</ProfileAdressDesc> 
-                <img src={ProfileImg} onClick={onClickEditEmail}/>
-                </EditContainer>
-                <ProfileAdress>Placeholder, 00 - Place</ProfileAdress>
-            </ProfileAdressContainer>
-            <DeliveryHistText>Histórico de pedidos</DeliveryHistText>
-            <Line></Line>
-        </ContainerProfilePage>
+        <div>{loadingScreen()}</div>
     )
 }
 
